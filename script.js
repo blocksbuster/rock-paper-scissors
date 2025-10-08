0caca084d04237371a1ef8cf7207313617776add
blocksbuster <61367265+blocksbuster@users.noreply.github.com>
2025-10-08 12:10:45 -0400

    Finish function getComputerChoice
    Now the function actually print's out Rock, Paper or Scissors randomly.

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 function getComputerChoice() {
-    let max = 3;
-    return Math.floor(Math.random() * max);
-    // console.log()
-}
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    let randomNumber = getRandomInt(3); 
+    
+    if (randomNumber === 0) {
+        return String("Rock")
+    }
+    else if (randomNumber === 1) {
+        return String("Paper")
+    }
+    else (randomNumber === 2) ;{
+        return String("Scissor")
+    }
+
+    }
+
+
+    // function computerChoice() {
+    //     if (computerChoice = 0) {
+    //         let computerChoice = "Rock"
+    //         return computerChoice
+    //     }
+    //     else if (computerChoice = 1) {
+    //         let computerChoice = "Paper"
+    //         return computerChoice
+    //     }
+    //     else (computerChoice = 2) {
+    //         let computerChoice = "Scissor"
+    //         return computerChoice
+    //     }
+
+
+    
 
 let random = getComputerChoice()
 console.log(random)
